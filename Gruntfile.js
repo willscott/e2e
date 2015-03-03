fb1e07787860e2e1bdd8c04b1867d2e256ec41cb
soycode <soycode@users.noreply.github.com>
2015-03-03 14:35:22 -0800

    v0.0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,8 +44,11 @@ module.exports = function(grunt) {
         } ]
       }
     },
+
+    clean: ['end-to-end/', 'end-to-end.build/']
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-force');
   grunt.loadNpmTasks('grunt-git');
